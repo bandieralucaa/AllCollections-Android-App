a167b109914b6ff7de72fe22622768ee97fb6e1d
bandieralucaa <lucabandiera2302@gmail.com>
2024-04-23 13:05:06 +0200

    Update CameraScreen

--- src/app/src/main/java/com/example/allcollections/screens/CameraScreen.kt
+++ src/app/src/main/java/com/example/allcollections/screens/CameraScreen.kt
@@ -6,9 +6,12 @@ import com.example.allcollections.utils.rememberPermission
 import android.Manifest
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.allcollections.navigation.Screens
 
 @Composable
 fun CameraScreen(navController: NavController) {
@@ -45,8 +49,33 @@ fun CameraScreen(navController: NavController) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
+
+        Text(
+            text = "Aggiungi Foto Profilo",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
         Button(onClick = ::takePicture) {
-            Text("Take a Picture")
+            Text("Scatta una foto")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = {
+        }) {
+            Text("Scegli dalla galleria")
+        }
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = {
+            navController.navigate(Screens.HomeScreen.name)
+        }) {
+            Text("In seguito")
         }
     }
 
